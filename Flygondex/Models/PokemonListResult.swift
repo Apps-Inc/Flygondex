b7899fe09e9d08dd8720facb04ced5f0465f9f3f
samuel <fsamuelsmartins@gmail.com>
2021-11-01 17:02:26 -0300

    Consuming Pokeapi
    #bulbasour

---
+++ Flygondex/Models/PokemonListResult.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonListResult.swift
+//  Flygondex
+//
+//  Created by Samuel on 01/11/21.
+//
+
+import Foundation
+
+struct PokemonListResult : Decodable{
+    let name : String
+    let url : String
+}
--- Flygondex/ViewController.swift
+++ Flygondex/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pokemonList: PokemonListRequest?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200")!
+        
+        
+        
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            
+            if let result = try? JSONDecoder().decode(PokemonListRequest.self, from: data) {
+                DispatchQueue.main.async {
+                    self.pokemonList = result
+                    self.tableView.reloadData()
+                    
+                }
+            } else {
+                print("error")
+            }
+            
+            
+        }.resume()
+
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("mudou o tamanho \(pokemonList?.count ?? 0)")
+        return pokemonList?.results.count  ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+                
+        print(indexPath.row)
+        cell.textLabel?.text = pokemonList?.results[indexPath.row].name
+        return cell
+    }
+    
 }
 
